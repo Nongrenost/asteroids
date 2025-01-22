a073d37fca607ee7bcd121989468f539856abb5a
Nongrenost <Nongrenost@gmail.com>
2025-01-22 21:45:53 +0300

    asteroids main game loop

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()  
+    lets_go = True
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while lets_go:
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+
+if __name__ == "__main__":
+    main()
