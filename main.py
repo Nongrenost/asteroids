f08b572ae2f2fe721cdb64b7974479f25215f9e6
Nongrenost <Nongrenost@gmail.com>
2025-01-27 17:22:09 +0300

    player fixed

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import pygame
-import pygame
 from constants import *
-from circleshape import CircleShape
 from player import Player
 
 def main():
     pygame.init() 
+    lets_go = True
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_time = pygame.time.Clock()
     delta_time = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_WIDTH / 2)
      
-    lets_go = True
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while lets_go:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        
         delta_time = game_time.tick(60) / 1000
 
 
